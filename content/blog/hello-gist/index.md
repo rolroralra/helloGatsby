7d82afeffcaad111b50e5088b71d44480e52034b
rolroralra <rolroralra@gmail.com>
2020-08-10 23:02:25 +0900

    first commit

---
+++ content/blog/hello-gist/index.md
@@ -0,0 +1,7 @@
+---
+title: Hello Gist
+date: "2015-05-30T22:12:03.284Z"
+description: "Hello Gist"
+---
+
+`gist:rolroralra/e807cc2507baa5d9d098e23eeee0430b#Github.md`
---
+++ gatsby-config.js
@@ -0,0 +1,110 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Starter rolroralra's Blog`,
+    author: {
+      name: `rolroralra`,
+      summary: `who just enjoy his own life in SEOUL, South Korea.`,
+    },
+    description: `A starter blog demonstrating what Gatsby can do.`,
+    siteUrl: `https://gatsby-starter-blog-demo.netlify.app/`,
+    social: {
+      twitter: `rolroralra`,
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          {
+            resolve: "gatsby-remark-embed-gist",
+            options: {
+              // Optional:
+
+              // the github handler whose gists are to be accessed
+              username: "rolroralra",
+
+              // a flag indicating whether the github default gist css should be included or not
+              // default: true
+              // DEPRECATED (PLEASE USE gistDefaultCssInclude)
+              includeDefaultCss: true,
+
+              // a flag indicating whether the github default gist css should be included or not
+              // default: true
+              gistDefaultCssInclude: true,
+
+              // a flag indicating whether the github default gist css should be preloaded or not
+              // use this if you want to load the default css asynchronously.
+              // default: false
+              gistCssPreload: false,
+
+              // a string that represents the github default gist css url.
+              // defaults: "https://github.githubassets.com/assets/gist-embed-b3b573358bfc66d89e1e95dbf8319c09.css"
+              gistCssUrlAddress: "https://github.githubassets.com/assets/gist-embed-b3b573358bfc66d89e1e95dbf8319c09.css"
+            }
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        //trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby Starter Blog`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `content/assets/gatsby-icon.png`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
